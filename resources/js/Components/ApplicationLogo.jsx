52f018e62a07847950537bee80d16d038fcba9c8
SebastianDeFranco <sebastianedefranco@gmail.com>
2023-10-24 16:57:02 -0300

    primer commit

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,9 @@
+import logo from '../../../public/logo.png'
+
+export default function ApplicationLogo(props) {
+    return (
+        <>
+            <img src={logo} alt="" className='logo' />
+        </>
+    );
+}
